e8890f4656f10a895eb4f274ec593f87f8ececaf
Spiro94 <danieljosevillamizar@gmail.com>
2025-08-06 10:39:20 -0500

    feat: Add product flavors for development, staging, and production environments; update launch configurations and app name resource

--- app/lib/inside/i18n/translations.g.dart
+++ app/lib/inside/i18n/translations.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 1
 /// Strings: 53
 ///
-/// Built on 2025-08-04 at 15:35 UTC
+/// Built on 2025-08-06 at 15:36 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
